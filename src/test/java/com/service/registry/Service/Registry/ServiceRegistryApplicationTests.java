12409018ea8b7c4185dd4db4f11ce57bbe373801
Shubham Vats <shubham_vats@192.168.1.8>
2024-10-24 11:44:35 +0530

    service registry implemented

---
+++ src/test/java/com/service/registry/Service/Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.registry.Service.Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
